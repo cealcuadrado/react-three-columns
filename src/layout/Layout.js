3a8e0ba2ec2a79f9cbb81137c189b3cf81ba2fe3
CeAlCuadrado <cealcuadrado@outlook.com>
2021-09-30 19:33:50 -0300

    Instaladas dependencias, creado layout

---
+++ src/layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <p>Hola, mundo!</p>
+        )
+    }
+}
+
+export default Layout;
